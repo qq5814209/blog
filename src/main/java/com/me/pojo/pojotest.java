ddfd6649cb9d51b29d5d73d27529730ac533bc92
qq5814209 <fe_email@163.com>
2019-06-03 15:30:29 +0800

    添加属性

--- src/main/java/com/me/pojo/pojotest.java
+++ src/main/java/com/me/pojo/pojotest.java
@@ -1,4 +1,22 @@
 package com.me.pojo;
 
 public class pojotest {
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
