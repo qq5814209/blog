1b992b4e1f580fbd2d5016eed44528251a94bffc
qq5814209 <fe_email@163.com>
2019-06-09 21:41:01 +0800

    实习简单登录功能

--- src/main/java/com/me/controller/UserInfoController.java
+++ src/main/java/com/me/controller/UserInfoController.java
@@ -5,10 +5,12 @@ import com.me.service.UserInfoService;
 import com.me.vo.UserInfoVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -29,10 +31,23 @@ public class UserInfoController {
 
     @ResponseBody
     @RequestMapping(value = "userLogin",method = RequestMethod.POST)
-    public Object login(String user,String password){
+    public Object login(String user, String password,HttpSession session){
         System.out.println(user + " : "+password);
         UserInfo userInfo = userInfoService.login(user,password);
         System.out.println(userInfo);
+        session.setAttribute("userInfo",userInfo);
+        return userInfo;
+    }
+
+    /**
+     * 判断是否登录
+     * @param session
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "islogin",method = RequestMethod.GET)
+    public Object islogin(HttpSession session){
+        UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
         return userInfo;
     }
 
