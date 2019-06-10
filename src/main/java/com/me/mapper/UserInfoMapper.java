f978899c9c46e6000d07a09db56f15670c491283
qq5814209 <fe_email@163.com>
2019-06-10 20:54:35 +0800

    注册表单验证实现

--- src/main/java/com/me/mapper/UserInfoMapper.java
+++ src/main/java/com/me/mapper/UserInfoMapper.java
@@ -1,6 +1,7 @@
 package com.me.mapper;
 
 import com.me.pojo.UserInfo;
+import com.me.vo.UserInfoVo;
 
 /**
  * 操作 userInfo 表
@@ -14,5 +15,10 @@ public interface UserInfoMapper {
      */
     UserInfo login(UserInfo userInfo);
 
-
+    /**
+     * 注册
+     * @param userInfoVo
+     * @return
+     */
+    int regiester(UserInfoVo userInfoVo);
 }
--- src/main/java/com/me/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/me/service/impl/UserInfoServiceImpl.java
@@ -3,6 +3,7 @@ package com.me.service.impl;
 import com.me.mapper.UserInfoMapper;
 import com.me.pojo.UserInfo;
 import com.me.service.UserInfoService;
+import com.me.vo.UserInfoVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,9 +24,28 @@ public class UserInfoServiceImpl implements UserInfoService {
      */
     public UserInfo login(String user, String password) {
         UserInfo userInfo = new UserInfo();
-        userInfo.setUser_name(user);
+        if(user.contains("@")){
+            userInfo.setEmail(user); //邮箱登录
+        }else {
+            userInfo.setUser_name(user); //账号登录
+        }
         userInfo.setPassword(password);
         return userInfoMapper.login(userInfo);
+    }
+
+    /**
+     * 注册
+     * @param user_name
+     * @param email
+     * @param password
+     * @return
+     */
+    public int regiester(String user_name, String email, String password) {
+        UserInfoVo userInfoVo = new UserInfoVo();
+        userInfoVo.setUser_name(user_name);
+        userInfoVo.setEmail(email);
+        userInfoVo.setPassword(password);
+        return userInfoMapper.regiester(userInfoVo);
     }
 
 
