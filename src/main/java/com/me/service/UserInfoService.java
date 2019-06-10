db26aacb429896472eb3b73186276ce39e2a30b0
pei <96078226@qq.com>
2019-06-10 10:17:15 +0800

    合并到UserInfo各层中

--- src/main/java/com/me/service/UserInfoService.java
+++ src/main/java/com/me/service/UserInfoService.java
@@ -1,7 +1,10 @@
 package com.me.service;
 
 import com.me.pojo.UserInfo;
+import com.me.vo.FavoritesVo;
 import com.me.vo.UserInfoVo;
+
+import java.util.List;
 
 /**
  * 查询用户信息、修改用户信息用
@@ -22,4 +25,18 @@ public interface UserInfoService {
      * @return
      */
     UserInfoVo selectUserInfo(int userId);
+
+    /**
+     * 根据用户id查询该用户的收藏夹信息
+     * @param userId
+     * @return
+     */
+    List<FavoritesVo> selectFavoritesByUserId(int userId);
+
+    /**
+     * 根据收藏夹id查询收藏夹中内容
+     * @param favorites_id
+     * @return
+     */
+    List<FavoritesVo> selectFavoritesByFavoritesId(int favorites_id);
 }
