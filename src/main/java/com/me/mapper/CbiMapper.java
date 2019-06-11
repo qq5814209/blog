0843761496716a24a48d68d99be482d77a7b6547
shenyao2333 <shenyao2333@163.com>
2019-06-11 17:17:26 +0800

    添加注释

--- src/main/java/com/me/mapper/CbiMapper.java
+++ src/main/java/com/me/mapper/CbiMapper.java
@@ -6,7 +6,18 @@ import com.me.pojo.Vip;
 import java.util.List;
 
 public interface CbiMapper {
+    /**
+     * 获取C币套餐
+     * @param cbi
+     * @return
+     */
     public List<Cbi> getCbi(Cbi cbi);
 
+
+    /**
+     * 获取指定的套餐
+     * @param Cbi_id
+     * @return
+     */
     public Cbi getCbiById(String Cbi_id);
 }
--- src/main/java/com/me/mapper/OrderMapper.java
+++ src/main/java/com/me/mapper/OrderMapper.java
@@ -3,10 +3,25 @@ package com.me.mapper;
 import com.me.pojo.Order;
 
 public interface OrderMapper {
+    /**
+     * 创建订单
+     * @param order
+     * @return
+     */
     public int setOrder(Order order);
 
+    /**
+     * 更改订单状态
+     * @param order
+     */
     public void upOrder(Order order);
 
+
+    /**
+     * 根据订单号获取订单
+     * @param order
+     * @return
+     */
     public Order getOrderByNum(Order order);
 
 
