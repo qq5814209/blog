8167a9f5f9e7132ef12d67217a2f6b6aeacc000e
pei <96078226@qq.com>
2019-06-12 23:06:39 +0800

    评论消息动态渲染完成

---
+++ src/main/java/com/me/service/MessageService.java
@@ -0,0 +1,14 @@
+package com.me.service;
+
+import com.me.dto.InformCommentDto;
+
+import java.util.List;
+
+public interface MessageService {
+    /**
+     * 查询评论消息的信息
+     * @param user_id
+     * @return
+     */
+    List<InformCommentDto> selectCommentByUserId(int user_id);
+}
